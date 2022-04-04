7b67992b53d477446029a821af14c47af84365fe
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-04-05 00:00:48 +0600

    implemented grpc server side

---
+++ server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bookshop/server/server/pb"
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedInventoryServer
+}
+
+func (s *server) GetBookList(ctx context.Context, in *pb.GetBookListRequest) (*pb.GetBookListResponse, error) {
+	log.Printf("Received request: %v", in.ProtoReflect().Descriptor().FullName())
+	return &pb.GetBookListResponse{
+		Books: getSampleBooks(),
+	}, nil
+}
+
+func getSampleBooks() []*pb.Book {
+	sampleBooks := []*pb.Book{
+		{
+			Title:     "Golang Play projects",
+			Author:    "Bekmuratov Azamat",
+			PageCount: 42,
+		},
+		{
+			Title:     "The Lord of the Rings",
+			Author:    "J.R.R. Tolkien",
+			PageCount: 1234,
+		},
+	}
+	return sampleBooks
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterInventoryServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("fatal to serve: %v", err)
+	}
+}
